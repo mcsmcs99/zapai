f5e80efb674a386afb937e3bf1c57290066be717
Matheus Correia dos Santos <mcsmatheusmcs99@gmail.com>
2025-08-28 20:01:13 -0300

    feat - create routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,7 +3,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: '/perfil', component: () => import('pages/ProfilePage.vue') },
+      { path: '/dashboard', component: () => import('pages/Dashboard.vue') },
+      { path: '/assistentes', component: () => import('pages/AssistantsList.vue') },
+      { path: '/conversas', component: () => import('pages/ConversationsPage.vue') },
+      { path: '/simulador', component: () => import('pages/SimulatorPage.vue') },
+      { path: '/admin', component: () => import('pages/AdminDashboard.vue') },
+      { path: '/empresas', component: () => import('pages/CompaniesPage.vue') },
     ]
   },
 
